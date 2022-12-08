4de0a870a4c75b6d03ef76733bffd99dc009242a
Güven Karabulut <56564068+guven07@users.noreply.github.com>
2022-12-08 21:42:37 +0300

    updated miniWeather

--- weather-project/assets/js/script.js
+++ weather-project/assets/js/script.js
@@ -1,10 +1,7 @@
 const url='https://api.openweathermap.org/data/2.5/'
-const key='7877f6563f0c7e57b230144397f14b37'
+const key='<URL KEY HERE>'
 
 function setQuery(){
-    //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-    //https://api.openweathermap.org/data/2.5weather?q=Ankara&appid=7877f6563f0c7e57b230144397f14b37&units=metric&lang=tr
-    //let query = ('${url}weather?q=${5}&appid=${key}&units=metric&lang=tr')
     let query=(url+'weather?q='+inputText.value+'&appid='+key+'&units=metric&lang=tr')
     console.log(query);
      fetch(query)
@@ -15,9 +12,19 @@ function setQuery(){
 }
 function displayResult(result){
     console.log(result);
+    const{name}=result;
+    const{icon,description}=result.weather[0];
+    const{temp,humidity}=result.main;
+
+    document.getElementById('sehir').innerHTML=`Şehir:${name}`
+    document.getElementById('durum').innerHTML=`Durum:${description}`
+    document.getElementById('nem').innerHTML=`Nem:${humidity}`
+    document.getElementById('sicaklik').innerHTML=`Sıcaklık:${temp}`
+    document.getElementById('image').setAttribute("src","http://openweathermap.org/img/wn/"+icon+"@2x.png")
 }
 
 const inputText=document.getElementById('textBar')
 const button=document.getElementById('button-addon1')
+
 
 button.addEventListener('click',setQuery)
